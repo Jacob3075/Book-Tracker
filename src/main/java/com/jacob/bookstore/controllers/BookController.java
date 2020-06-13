bfa300723baead256c7dbe0fc7d567e01caba13c
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-06-13 10:46:51 +0530

    Added BookController and its endpoints test file

---
+++ src/main/java/com/jacob/bookstore/controllers/BookController.java
@@ -0,0 +1,55 @@
+package com.jacob.bookstore.controllers;
+
+import com.jacob.bookstore.models.Book;
+import com.jacob.bookstore.services.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api.book-store/")
+public class BookController {
+
+	private final BookService bookService;
+
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+
+	@GetMapping("/books/")
+	public List<Book> getBooks() {
+		return bookService.findAll();
+	}
+
+	@GetMapping(value = "books/{id}/")
+	public Optional<Book> getBookById(@PathVariable Long id) {
+		return bookService.findById(id);
+	}
+
+	@PostMapping("/books")
+	public String createBook(@RequestBody Book book) {
+		bookService.saveAndFlush(book);
+		return "Created";
+	}
+
+	@DeleteMapping("/books/{id}")
+	public void deleteBook(@PathVariable Long id, HttpServletResponse response) {
+		if (bookService.deleteById(id)) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+
+	}
+
+	@PutMapping("books/{id}")
+	public void updateBook(@PathVariable Long id, @RequestBody Book book, HttpServletResponse response) {
+		if (bookService.updateBook(id, book)) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+}
