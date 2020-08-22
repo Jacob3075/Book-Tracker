8fcf651e5bf2814cbcc677c237c8ac9bc6ef9af4
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-08-22 11:41:50 +0530

    Updated AuthorController and AuthorService to use functional reactive endpoints

--- src/main/java/com/jacob/booktracker/controllers/AuthorController.java
+++ src/main/java/com/jacob/booktracker/controllers/AuthorController.java
@@ -1,12 +1,14 @@
 package com.jacob.booktracker.controllers;
 
-import com.jacob.booktracker.models.Author;
 import com.jacob.booktracker.services.AuthorService;
-import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
-@RestController
-@RequestMapping(value = "/api.book-store/authors")
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.nest;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
 public class AuthorController {
 	private final AuthorService authorService;
 
@@ -14,54 +16,16 @@ public class AuthorController {
 		this.authorService = authorService;
 	}
 
-	@GetMapping(value = "/")
-	public Flux<Author> getAuthors() {
-		return authorService.findAll();
+	@Bean
+	public RouterFunction<ServerResponse> authorEndpoints() {
+		return nest(
+				path("/api.book-store/authors"),
+				route(GET("/"), authorService::findAll)
+						.andRoute(POST("/"), authorService::addNewAuthor)
+						.andRoute(PUT("/"), authorService::updateAuthor)
+						.andRoute(GET("{id}"), authorService::findById)
+						.andRoute(DELETE("{id}"), authorService::deleteById)
+		);
 	}
 
-	@GetMapping(value = "/{id}")
-	public Author getAuthorById(@PathVariable String id) {
-//		Optional<Author> optionalAuthor = authorService.findById(id);
-//		if (optionalAuthor.isPresent()) {
-//			response.setStatus(HttpServletResponse.SC_OK);
-//			return optionalAuthor.get();
-//		} else {
-//			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-//			return null;
-//		}
-		return null;
-	}
-
-	@PostMapping(value = "/")
-	public String addNewAuthor(@RequestBody Author author) {
-//		if (authorService.addNewAuthor(author)) {
-//			response.setStatus(HttpServletResponse.SC_CREATED);
-//			return "CREATED";
-//		} else {
-//			response.setStatus(HttpServletResponse.SC_CONFLICT);
-			return "ALREADY EXISTS";
-//		}
-	}
-
-	@DeleteMapping(value = "/{id}")
-	public String deleteAuthor(@PathVariable String id) {
-//		if (authorService.deleteById(id)) {
-//			response.setStatus(HttpServletResponse.SC_OK);
-//			return "DELETED";
-//		} else {
-//			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			return "NOT FOUND";
-//		}
-	}
-
-	@PutMapping(value = "/{id}")
-	public String updateAuthor(@PathVariable String id, @RequestBody Author newAuthor) {
-//		if (authorService.updateAuthor(id, newAuthor)) {
-//			response.setStatus(HttpServletResponse.SC_OK);
-//			return "UPDATED";
-//		} else {
-//			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			return "NOT FOUND";
-//		}
-	}
 }
--- src/main/java/com/jacob/booktracker/services/AuthorService.java
+++ src/main/java/com/jacob/booktracker/services/AuthorService.java
@@ -3,10 +3,11 @@ package com.jacob.booktracker.services;
 import com.jacob.booktracker.models.Author;
 import com.jacob.booktracker.repositories.AuthorRepository;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Service
 public class AuthorService {
@@ -17,36 +18,25 @@ public class AuthorService {
 		this.authorRepository = authorRepository;
 	}
 
-	public Flux<Author> findAll() {
-		return authorRepository.findAll();
+	public Mono<ServerResponse> findAll(ServerRequest serverRequest) {
+		return ok().body(authorRepository.findAll(), Author.class);
 	}
 
-	public void deleteById(String id) {
-		this.findById(id)
-		    .subscribe(author -> authorRepository.deleteById(id));
-//		if (optionalAuthor.isPresent()) {
-//			authorRepository.deleteById(id);
-//			return true;
-//		} else {
-//			return false;
-//		}
-//		return true;
+	public Mono<ServerResponse> findById(ServerRequest serverRequest) {
+		return ok().body(authorRepository.findById(serverRequest.pathVariable("id")), Author.class);
 	}
 
-	public Mono<Author> findById(String id) {
-		return authorRepository.findById(id);
+	public Mono<ServerResponse> addNewAuthor(ServerRequest serverRequest) {
+		return ok().body(serverRequest.bodyToMono(Author.class)
+		                              .flatMap(authorRepository::save), Author.class);
 	}
 
-	public boolean updateAuthor(String id, Author newAuthor) {
-		return Author.stream(List.of(newAuthor))
-		             .updateAuthor(id, authorRepository)
-		             .saveAuthor(authorRepository);
+	public Mono<ServerResponse> updateAuthor(ServerRequest serverRequest) {
+		return ok().body(serverRequest.bodyToMono(Author.class)
+		                              .flatMap(authorRepository::save), Author.class);
 	}
 
-	public boolean addNewAuthor(Author author) {
-		return Author.stream(List.of(author))
-		             .ifNewAuthor(authorRepository)
-		             .saveAuthor(authorRepository);
-
+	public Mono<ServerResponse> deleteById(ServerRequest serverRequest) {
+		return ok().body(authorRepository.deleteById(serverRequest.pathVariable("id")), Void.class);
 	}
 }
