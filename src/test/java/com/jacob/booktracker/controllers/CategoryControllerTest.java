f72216f7ea3dd3c0c960c4df7a3ac2e6b6298687
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-08-24 11:17:34 +0530

    Added CategoryControllerTest

--- src/test/java/com/jacob/booktracker/controllers/CategoryControllerTest.java
+++ src/test/java/com/jacob/booktracker/controllers/CategoryControllerTest.java
@@ -1,120 +1,146 @@
 package com.jacob.booktracker.controllers;
 
-import com.google.gson.Gson;
-import com.jacob.booktracker.dtos.response.CategoryDTO;
+import com.jacob.booktracker.config.MongoReactiveApplication;
 import com.jacob.booktracker.models.Category;
+import com.jacob.booktracker.repositories.CategoryRepository;
 import com.jacob.booktracker.services.CategoryService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-@WebMvcTest(controllers = CategoryController.class)
+@WebFluxTest(controllers = CategoryController.class)
+@ExtendWith(SpringExtension.class)
+@Import({CategoryService.class, MongoReactiveApplication.class, CategoryController.class})
 class CategoryControllerTest {
-
-	private final List<CategoryDTO> categoryList = new ArrayList<>();
-	private final String            urlTemplate  = "/api.book-store/categories/";
-
-	@Autowired
-	private MockMvc mockMvc;
+	private final List<Category> categoryList = new ArrayList<>();
+	private final String      baseUrl    = "http://localhost:8080/api.book-store/categories/";
 
 	@MockBean
-	private CategoryService categoryService;
+	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@BeforeEach
 	void setUp() {
-		categoryList.clear();
+		Category category1 = new Category();
+		Category category2 = new Category();
 
-		categoryList.addAll(Arrays.asList(
-				new CategoryDTO("1", "Category 1"),
-				new CategoryDTO("2L", "Category 2"),
-				new CategoryDTO("3L", "Category 3"),
-				new CategoryDTO("4L", "Category 4"),
-				new CategoryDTO("5L", "Category 4")
-		));
+		category1.setId("1");
+		category2.setId("1");
 
+		category1.setCategoryName("Category 1");
+		category2.setCategoryName("Category 2");
 
+		category1.setBookIds(Set.of("1", "2", "3"));
+		category2.setBookIds(Set.of("1", "2", "3"));
+
+		categoryList.add(category1);
+		categoryList.add(category2);
 	}
 
 	@Test
-	void getAllCategories() throws Exception {
+	void getAllCategoriesTest() {
+		given(categoryRepository.findAll()).willReturn(Flux.fromIterable(categoryList));
 
-		given(categoryService.findAll()).willReturn(categoryList);
-
-		mockMvc.perform(get(urlTemplate))
-		       .andExpect(status().isOk())
-		       .andExpect(jsonPath("$.size()", is(categoryList.size())))
-		       .andExpect(jsonPath("$[0].categoryName", is(categoryList.get(0).getCategoryName())));
-
+		webTestClient.get()
+		             .uri(baseUrl)
+		             .exchange()
+		             .expectStatus().isOk()
+		             .expectBody()
+		             .jsonPath("$").isArray()
+		             .jsonPath("$[0].id").isEqualTo(categoryList.get(0).getId())
+		             .jsonPath("$[0].categoryName").isEqualTo(categoryList.get(0).getCategoryName())
+		             .jsonPath("$[0].bookIds").isArray();
+		
+		verify(categoryRepository, times(1)).findAll();
 	}
 
 	@Test
-	void findById() throws Exception {
-		given(categoryService.findById("1L")).willReturn(Optional.ofNullable(categoryList.get(0)));
-		given(categoryService.findById("2L")).willReturn(Optional.empty());
+	void getCategoryByIdTest() {
+		Category category = categoryList.get(0);
+		given(categoryRepository.findById(category.getId())).willReturn(Mono.just(category));
 
-		mockMvc.perform(get(urlTemplate + "1"))
-		       .andExpect(status().isOk())
-		       .andExpect(jsonPath("$.categoryName", is(categoryList.get(0).getCategoryName())))
-		       .andExpect(jsonPath("$.id", is(categoryList.get(0).getId())));
+		webTestClient.get()
+		             .uri(baseUrl + category.getId())
+		             .exchange()
+		             .expectStatus().isOk()
+		             .expectBody()
+		             .jsonPath("$.id").isEqualTo(category.getId())
+		             .jsonPath("$.categoryName").isEqualTo(category.getCategoryName())
+		             .jsonPath("$.bookIds").isArray();
 
-		mockMvc.perform(get(urlTemplate + "2"))
-		       .andExpect(status().isNotFound())
-		       .andExpect(jsonPath("$").doesNotExist());
-
+		verify(categoryRepository, times(1)).findById(category.getId());
 	}
 
 	@Test
-	void addNewCategory() throws Exception {
-//		Category category = CommonUtils.convertFromCategoryDTO(categoryList.get(1));
-		Category category = new Category();
-		given(categoryService.addNewCategory(category)).willReturn(true);
+	void addNewCategoryTest() {
+		Category category = categoryList.get(0);
+		given(categoryRepository.save(any())).willReturn(Mono.just(category));
 
-		mockMvc.perform(post(urlTemplate).contentType(MediaType.APPLICATION_JSON)
-		                                 .content(new Gson().toJson(category)))
-		       .andExpect(status().isOk())
-		       .andExpect(jsonPath("$", is("CREATED")));
+		webTestClient.post()
+		             .uri(baseUrl)
+		             .contentType(MediaType.APPLICATION_JSON)
+		             .body(BodyInserters.fromValue(category))
+		             .exchange()
+		             .expectStatus().isOk()
+		             .expectBody()
+		             .jsonPath("$.id").isEqualTo(category.getId())
+		             .jsonPath("$.categoryName").isEqualTo(category.getCategoryName())
+		             .jsonPath("$.bookIds").isArray();
 
-		given(categoryService.addNewCategory(category)).willReturn(false);
-
-		mockMvc.perform(post(urlTemplate).contentType(MediaType.APPLICATION_JSON)
-		                                 .content(new Gson().toJson(category)))
-		       .andExpect(status().isNotFound())
-		       .andExpect(jsonPath("$", is("NOT FOUND")));
+		verify(categoryRepository, times(1)).save(any());
 	}
 
 	@Test
-	void deleteCategory() throws Exception {
+	void updateCategoryTest() {
+		Category category = categoryList.get(0);
+		given(categoryRepository.save(any())).willReturn(Mono.just(category));
 
-		given(categoryService.deleteById("1L")).willReturn(true);
-		given(categoryService.deleteById("2L")).willReturn(false);
+		webTestClient.put()
+		             .uri(baseUrl)
+		             .contentType(MediaType.APPLICATION_JSON)
+		             .body(BodyInserters.fromValue(category))
+		             .exchange()
+		             .expectStatus().isOk()
+		             .expectBody()
+		             .jsonPath("$.id").isEqualTo(category.getId())
+		             .jsonPath("$.categoryName").isEqualTo(category.getCategoryName())
+		             .jsonPath("$.bookIds").isArray();
 
-		mockMvc.perform(delete(urlTemplate + "1"))
-		       .andExpect(status().isOk())
-		       .andExpect(jsonPath("$", is("DELETED")));
-
-		mockMvc.perform(delete(urlTemplate + "2"))
-		       .andExpect(status().isNotFound())
-		       .andExpect(jsonPath("$", is("NOT FOUND")));
-
-
+		verify(categoryRepository, times(1)).save(any());
 	}
 
 	@Test
-	void updateCategory() {
+	void deleteCategoryTest() {
+		Category category = categoryList.get(0);
+		given(categoryRepository.deleteById(category.getId())).willReturn(Mono.empty());
+
+		webTestClient.delete()
+		             .uri(baseUrl + category.getId())
+		             .exchange()
+		             .expectStatus().isOk()
+		             .expectBody().isEmpty();
+
+		verify(categoryRepository, times(1)).deleteById(category.getId());
 	}
 }
