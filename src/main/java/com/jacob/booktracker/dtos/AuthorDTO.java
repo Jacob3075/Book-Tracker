c3703d0663d679c32bc1938e3a35b9d196c6731b
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-08-25 12:29:53 +0530

    Added DTO's for models

---
+++ src/main/java/com/jacob/booktracker/dtos/AuthorDTO.java
@@ -0,0 +1,15 @@
+package com.jacob.booktracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorDTO {
+	private String id;
+	private String authorName;
+}
---
+++ src/main/java/com/jacob/booktracker/dtos/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.jacob.booktracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+	private String id;
+	private String categoryName;
+}
