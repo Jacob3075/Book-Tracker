d75b679bf7fcf50fc5c89192975031dba69b6433
Jacob Bosco <jacob.bosco3075@gmail.com>
2020-08-27 11:43:15 +0530

    Starting to implement BookService.addNewBook to use CombinedDtoMono to add the new books

--- src/main/java/com/jacob/booktracker/services/BookService.java
+++ src/main/java/com/jacob/booktracker/services/BookService.java
@@ -1,7 +1,13 @@
 package com.jacob.booktracker.services;
 
+import com.jacob.booktracker.dtos.BookDTO;
+import com.jacob.booktracker.dtos.CombinedDTO;
 import com.jacob.booktracker.models.Book;
+import com.jacob.booktracker.repositories.AuthorRepository;
 import com.jacob.booktracker.repositories.BookRepository;
+import com.jacob.booktracker.repositories.CategoryRepository;
+import com.jacob.booktracker.utils.CommonUtils;
+import com.jacob.booktracker.utils.mono.CombinedDtoMono;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -12,11 +18,17 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 @Service
 public class BookService {
 
-	private final BookRepository bookRepository;
+	private final BookRepository     bookRepository;
+	private final AuthorRepository   authorRepository;
+	private final CategoryRepository categoryRepository;
 
 	public BookService(
-			BookRepository bookRepository) {
+			BookRepository bookRepository,
+			AuthorRepository authorRepository,
+			CategoryRepository categoryRepository) {
 		this.bookRepository = bookRepository;
+		this.authorRepository = authorRepository;
+		this.categoryRepository = categoryRepository;
 	}
 
 	public Mono<ServerResponse> findAll(ServerRequest serverRequest) {
@@ -37,10 +49,33 @@ public class BookService {
 	}
 
 	public Mono<ServerResponse> addNewBook(ServerRequest serverRequest) {
-		return ok().body(
-				serverRequest.bodyToMono(Book.class)
-				             .flatMap(bookRepository::save),
-				Book.class
-		);
+		Mono<CombinedDTO> combinedDTOMono = serverRequest.bodyToMono(BookDTO.class)
+		                                                 .map(CommonUtils::getCombinedDto);
+		new CombinedDtoMono(combinedDTOMono)
+				.saveAuthors(authorRepository)
+				.saveCategories(categoryRepository)
+				.saveBook(bookRepository)
+				.getResponseDto();
+
+
+//		serverRequest.bodyToMono(BookDTO.class)
+//		             .map(CommonUtils::getCombinedDto)
+//		             .map(combinedDTO -> {
+//			             Book.mono(Mono.just(combinedDTO.getBook()))
+//			                 .checkIfBookAlreadyExists(bookRepository)
+//			                 .saveBook(bookRepository)
+//			                 .getResponseDto();
+//		             })
+
+		return Book.mono(serverRequest.bodyToMono(Book.class))
+		           .checkIfBookAlreadyExists(bookRepository)
+		           .saveBook(bookRepository)
+		           .getResponseDto();
+
+//		return ok().body(
+//				serverRequest.bodyToMono(Book.class)
+//				             .flatMap(bookRepository::save),
+//				Book.class
+//		);
 	}
 }
